10a420bdfde3c473fdb9af170ef4a43a6d8c66f9
k4tie113 <katieli167@gmail.com>
2025-10-05 15:32:26 -0500

    made a website that queries semantic scholar

---
+++ website/backend/services/semantic_api.py
@@ -0,0 +1,21 @@
+import requests
+
+def search_semantic(query):
+    """Query Semantic Scholar API and return normalized results."""
+    url = f"https://api.semanticscholar.org/graph/v1/paper/search?query={query}&limit=5&fields=title,year,authors,url,abstract"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        return [{"title": "Error fetching papers", "authors": [], "year": None, "abstract": "", "url": ""}]
+
+    data = res.json()
+    results = []
+    for paper in data.get("data", []):
+        results.append({
+            "title": paper.get("title", "Untitled"),
+            "authors": [a["name"] for a in paper.get("authors", [])],
+            "year": paper.get("year"),
+            "abstract": paper.get("abstract", ""),
+            "url": paper.get("url", "")
+        })
+    return results
